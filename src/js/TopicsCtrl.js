2e41a05ab5056442d57552a089b4931653525390
Satake <satake@objectfoo.com>
2014-04-21 15:49:01 -0700

    minification save constructors

--- src/js/TopicsCtrl.js
+++ src/js/TopicsCtrl.js
@@ -1,28 +1,28 @@
 define(['app', 'topicStorage'], function (app) {
 	'use strict';
 
-	return app.controller('TopicsCtrl', function ($scope, $attrs, topicStorage, topicKeys) {
-		var topicID = $attrs.id;
-
-		// save a list of topic keys for printing
-		if (!~topicKeys.indexOf(topicID)) {
-			topicKeys.push(topicID);
-		}
-
-		$scope.model = {
-			topicID: topicID,
-			topics: topicStorage.get(topicID)
-		};
-
-		$scope.$watch('model.topics', updateStore, true);
-		$scope.$on('topics.clear', clearTopics);
-
-		function updateStore() {
-			topicStorage.put(topicID, $scope.model.topics);
-		}
-
-		function clearTopics() {
-			$scope.model.topics.length = 0;
-		}
-	});
+	return app.controller('TopicsCtrl', ['$scope', '$attrs', 'topicStorage', 'topicKeys', function ($scope, $attrs, topicStorage, topicKeys) {
+			var topicID = $attrs.id;
+	
+			// save a list of topic keys for printing
+			if (!~topicKeys.indexOf(topicID)) {
+				topicKeys.push(topicID);
+			}
+	
+			$scope.model = {
+				topicID: topicID,
+				topics: topicStorage.get(topicID)
+			};
+	
+			$scope.$watch('model.topics', updateStore, true);
+			$scope.$on('topics.clear', clearTopics);
+	
+			function updateStore() {
+				topicStorage.put(topicID, $scope.model.topics);
+			}
+	
+			function clearTopics() {
+				$scope.model.topics.length = 0;
+			}
+		}]);
 });
