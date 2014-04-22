ee375a704c9f70b928b7a58ce1645bd967c486a2
Satake <satake@objectfoo.com>
2014-04-21 17:55:04 -0700

    the delete

--- src/js/app.js
+++
@@ -1,6 +0,0 @@
-var retrospective = angular.module('retrospective', [])
-	.value('topicKeys', [])
-	.controller('MainCtrl', function MainCtrl($scope, $rootScope, topicStorage, topicKeys) {
-		'use strict';
-		$scope.today = new Date();
-	});
